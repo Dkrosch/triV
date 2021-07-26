bdb601a20df85e84e0f59041d8584b9d0d26d4e6
Vincentius Michael Tanakoman <59143319+pocounda@users.noreply.github.com>
2021-07-26 17:25:45 +0700

    Add files via upload

---
+++ GConnect/GConnect/Features/Login/View/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  GConnect
+//
+//  Created by Michael Tanakoman on 25/07/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ GConnect/GConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GConnect
+//
+//  Created by Michael Tanakoman on 25/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

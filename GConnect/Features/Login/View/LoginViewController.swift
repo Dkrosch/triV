b3fdc8fa153886957caa72b489c9aa917805b28b
Vincentius Michael Tanakoman <michael.tanakoman@gmail.com>
2021-07-27 18:06:40 +0700

    Ini file pertama kali

---
+++ GConnect/Features/Login/View/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  GConnect
+//
+//  Created by Michael Tanakoman on 27/07/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+}
---
+++ Pods/Target Support Files/Pods-GConnect/Pods-GConnect-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GConnect : NSObject
+@end
+@implementation PodsDummy_Pods_GConnect
+@end

605ec04522c24092e785a2455e07c424ea32a34d
Dkrosch <68946876+Dkrosch@users.noreply.github.com>
2021-08-02 23:51:38 +0700

    detail lounge owner

--- GConnect/Features/Detail Lounge/View/DetailLoungeViewController.swift
+++ GConnect/Features/Detail Lounge/View/DetailLoungeViewController.swift
@@ -16,6 +16,12 @@ class DetailLoungeViewController: UIViewController {
     @IBOutlet weak var roles: UICollectionView!
     @IBOutlet weak var DescriptionTextbox: UITextView!
     @IBOutlet weak var JoinLoungeButton: UIButton!
+    @IBOutlet weak var DeleteButton: UIButton!
+    @IBOutlet weak var gender: UITextField!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var genderLabelDesc: UILabel!
+    @IBOutlet weak var viewRole: UIView!
+    @IBOutlet weak var RoleButton: UIButton!
     
     var idLounge = ""
     //var detailLoungeVM = DetailLoungeViewModel()
@@ -28,12 +34,15 @@ class DetailLoungeViewController: UIViewController {
         Rank.layer.cornerRadius = 8
         Rank.layer.borderWidth = 2
         Rank.layer.borderColor = #colorLiteral(red: 0.9921568627, green: 0.5882352941, blue: 0.4666666667, alpha: 1)
+        gender.layer.cornerRadius = 8
+        gender.layer.borderWidth = 2
+        gender.layer.borderColor = #colorLiteral(red: 0.9921568627, green: 0.5882352941, blue: 0.4666666667, alpha: 1)
         Game.layer.cornerRadius = 8
         Game.layer.borderWidth = 2
         Game.layer.borderColor = #colorLiteral(red: 0.9921568627, green: 0.5882352941, blue: 0.4666666667, alpha: 1)
         DescriptionTextbox.layer.cornerRadius = 8
         JoinLoungeButton.layer.cornerRadius = 8
-
+        DeleteButton.isHidden = true
         DescriptionTextbox.layer.borderColor = UIColor.red.cgColor;
         
         let nibRoles = UINib(nibName: "\(RequirementExploreLoungeCollectionViewCell.self)", bundle: nil)
@@ -54,7 +63,33 @@ class DetailLoungeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = false
         self.navigationItem.title = "Detail Lounge"
+        gender.isHidden = true
+        genderLabel.isHidden = true
+        viewRole.isHidden = true
         //detailLoungeVM.getData(id: idLounge)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editTapped))
+    }
+    @objc func editTapped(){
+        DeleteButton.isHidden = false
+        JoinLoungeButton.isHidden = true
+        viewRole.isHidden = false
+        roles.isHidden = true
+        gender.isHidden = false
+        genderLabel.isHidden = false
+        genderLabelDesc.isHidden = true
+        Rank.layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        roles.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        roles.layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        DescriptionTextbox.layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneTapped))
+    }
+    
+    @objc func doneTapped(){
+        Rank.layer.backgroundColor = #colorLiteral(red: 0.1764705882, green: 0.2156862745, blue: 0.3254901961, alpha: 1)
+        roles.layer.backgroundColor = #colorLiteral(red: 0.1764705882, green: 0.2156862745, blue: 0.3254901961, alpha: 1)
+        genderLabelDesc.isHidden = false
+        DescriptionTextbox.layer.backgroundColor = #colorLiteral(red: 0.1764705882, green: 0.2156862745, blue: 0.3254901961, alpha: 1)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(editTapped))
     }
 }
 

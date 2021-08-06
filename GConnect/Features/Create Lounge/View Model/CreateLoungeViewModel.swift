033bde440ca3ac6e8f9a115d462cf1fe678b17b3
Vincentius Michael Tanakoman <michael.tanakoman@gmail.com>
2021-08-06 22:14:46 +0700

    fix chat

--- GConnect/Features/Create Lounge/View Model/CreateLoungeViewModel.swift
+++ GConnect/Features/Create Lounge/View Model/CreateLoungeViewModel.swift
@@ -20,7 +20,8 @@ class CreateLoungeViewModel{
         formatter.dateStyle = .long
         let currentTime = formatter.string(from: currentDateTime)
         
-        let db = Firestore.firestore()
+        let db = Firestore.firestore().collection("LoungeDetail").document()
+        let idLounge = db.documentID
         
         if game == ""{
             valid("game")
@@ -29,13 +30,22 @@ class CreateLoungeViewModel{
         }else if role == checkRole{
             valid("role")
         }else{
-            db.collection("LoungeDetail").document().setData(["CreateAt":currentTime, "Desc":desc, "Game":game, "Title":title, "idMemberLounge":["Member1":uid, "Member2":"", "Member3":"", "Member4":"", "Member5":"", "Member6":"", "Member7":"", "Member8":"", "Member9":"", "Member10":""], "idRequirementsLounge":["Sentinel":role[0], "Initiator":role[1], "Controller":role[2], "Duelist":role[3], ], "Gender":gender, "Rank":rank]) {(error) in
+            db.setData(["CreateAt":currentTime, "Desc":desc, "Game":game, "Title":title, "idMemberLounge":["Member1":uid, "Member2":"", "Member3":"", "Member4":"", "Member5":"", "Member6":"", "Member7":"", "Member8":"", "Member9":"", "Member10":""], "idRequirementsLounge":["Sentinel":role[0], "Initiator":role[1], "Controller":role[2], "Duelist":role[3], ], "Gender":gender, "Rank":rank]) {(error) in
                 if error != nil{
                     print("Gagal")
                 } else {
-                    print("Sukses")
+                    print("Sukses create lounge")
                 }
             }
+            
+            Firestore.firestore().collection("LoungeDetail").document(idLounge).collection("thread").document().collection("thread").document().setData(["content":"", "created":"", "id":"", "senderID":"", "senderName":""]){(error) in
+                if error != nil{
+                    print("Gagal")
+                } else {
+                    print("Sukses create chat lounge")
+                }
+            }
+            
             valid("true")
         }
     }

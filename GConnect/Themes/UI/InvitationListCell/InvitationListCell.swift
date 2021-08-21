218f689ff2e4a27842ae0dc877ccceea532bb6e7
Kevin Rivaldo <kevinrivaldo12.kr@gmail.com>
2021-08-22 00:22:24 +0700

    InvitationList terbaru

--- GConnect/Themes/UI/InvitationListCell/InvitationListCell.swift
+++ GConnect/Themes/UI/InvitationListCell/InvitationListCell.swift
@@ -8,7 +8,22 @@
 import UIKit
 
 class InvitationListCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var FromLabel: UILabel!
+    @IBOutlet weak var UserImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var userRoleLabel: UILabel!
+    @IBOutlet weak var loungeMemberqtyLbl: UILabel!
+    @IBOutlet weak var loungeTitleLabel: UILabel!
+    @IBOutlet weak var loungeDescLabel: UILabel!
+    @IBOutlet weak var requirementLabel: UILabel!
+    @IBOutlet weak var requirementLoungeCollectionView: UICollectionView!
+    
+    @IBOutlet weak var rejectBtn: UIButton!
+    @IBOutlet weak var AcceptBtn: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code

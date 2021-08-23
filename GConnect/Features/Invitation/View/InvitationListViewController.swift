48ec529a2f09f090cf5d9064ce76e340bee2a326
Kevin Rivaldo <kevinrivaldo12.kr@gmail.com>
2021-08-23 13:42:17 +0700

    Invitaion List terbaru

--- GConnect/Features/Invitation/View/InvitationListViewController.swift
+++ GConnect/Features/Invitation/View/InvitationListViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Firebase
 
-class InvitationListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class InvitationListViewController: UIViewController{
 
     @IBOutlet weak var invitaionListCollectionView: UICollectionView!
     override func viewDidLoad() {
@@ -17,51 +17,53 @@ class InvitationListViewController: UIViewController, UICollectionViewDelegate, 
         
         super.viewDidLoad()
 
-        invitaionListCollectionView.delegate = self
-        invitaionListCollectionView.dataSource = self
+//        invitaionListCollectionView.delegate = self
+//        invitaionListCollectionView.dataSource = self
     }
     
-    extension InvitationListViewController: UICollectionViewDataSource, UICollectionViewDelegate{
-        
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            if datas.count != 0{
-                return datas.count
-            }else{
-                return 1
-            }
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            if datas.count != 0{
-                let nib = UINib(nibName: "\(ExploreLoungeCell.self)", bundle: nil)
-                invitaionListCollectionView.register(nib, forCellWithReuseIdentifier: "loungeCollectionViewCell" )
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "loungeCollectionViewCell", for: indexPath) as! ExploreLoungeCell
-                cell.configureCell(detailLounge: datas[indexPath.row])
-                return cell
-            }else{
-                invitationListCollectionView.register(EmptyExploreLoungeCollectionViewCell.nib(), forCellWithReuseIdentifier: EmptyExploreLoungeCollectionViewCell.identifier)
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EmptyExploreLoungeCollectionViewCell.identifier, for: indexPath) as! EmptyExploreLoungeCollectionViewCell
-                return cell
-            }
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            if datas.count != 0{
-                let idLounge = datas[indexPath.row].documentId
-                performSegue(withIdentifier: "DetailLounge", sender: idLounge)
-            }else{
-                
-            }
-        }
-        
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if (segue.identifier == "DetailLounge"){
-                let loungeID = sender as? String
-                let vc = segue.destination as? DetailLoungeViewController
-                vc?.idLounge = loungeID!
-            }
-        }
-    }
+    
+    
+//    extension InvitationListViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+//
+//        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//            if datas.count != 0{
+//                return datas.count
+//            }else{
+//                return 1
+//            }
+//        }
+//
+//        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//            if datas.count != 0{
+//                let nib = UINib(nibName: "\(ExploreLoungeCell.self)", bundle: nil)
+//                invitaionListCollectionView.register(nib, forCellWithReuseIdentifier: "loungeCollectionViewCell" )
+//                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "loungeCollectionViewCell", for: indexPath) as! ExploreLoungeCell
+//                cell.configureCell(detailLounge: datas[indexPath.row])
+//                return cell
+//            }else{
+//                invitationListCollectionView.register(EmptyExploreLoungeCollectionViewCell.nib(), forCellWithReuseIdentifier: EmptyExploreLoungeCollectionViewCell.identifier)
+//                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EmptyExploreLoungeCollectionViewCell.identifier, for: indexPath) as! EmptyExploreLoungeCollectionViewCell
+//                return cell
+//            }
+//        }
+//
+//        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//            if datas.count != 0{
+//                let idLounge = datas[indexPath.row].documentId
+//                performSegue(withIdentifier: "DetailLounge", sender: idLounge)
+//            }else{
+//
+//            }
+//        }
+//
+//        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            if (segue.identifier == "DetailLounge"){
+//                let loungeID = sender as? String
+//                let vc = segue.destination as? DetailLoungeViewController
+//                vc?.idLounge = loungeID!
+//            }
+//        }
+//    }
     
    
    
